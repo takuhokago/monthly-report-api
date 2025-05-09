bbbd72e13e424ed5d51a89f0acff2b27f4cfab61
takuhokago <takuho1226000@gmail.com>
2025-05-09 20:26:24 +0900

    apiリポジトリ用に設定を変更（デバッグ起動OK）

--- src/main/resources/setup-database.sql
+++ src/main/resources/setup-database.sql
@@ -1,3 +1,8 @@
-CREATE DATABASE v8aaakav2nzn5tmh CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER 'nws2fapl45f8f0fg'@'v8aaakav2nzn5tmh' IDENTIFIED BY 'e6zct6a5q45zreli';
-GRANT ALL PRIVILEGES ON v8aaakav2nzn5tmh.* to 'nws2fapl45f8f0fg'@'v8aaakav2nzn5tmh';
+-- データベースの作成
+CREATE DATABASE monthly_report_api CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ユーザーの作成（パスワードは任意に変更可能）
+CREATE USER 'monthly_api_user'@'localhost' IDENTIFIED BY 'monthly_api_pass';
+
+-- 権限付与
+GRANT ALL PRIVILEGES ON monthly_report_api.* TO 'monthly_api_user'@'localhost';
