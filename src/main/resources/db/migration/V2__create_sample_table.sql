27aba6da8fd911de78c0b4d7c2c991857525624f
takuhokago <takku1226000@gmail.com>
2025-06-14 19:40:18 +0900

    revert v2SQLファイル

--- src/main/resources/db/migration/V2__create_sample_table.sql
+++ src/main/resources/db/migration/V2__create_sample_table.sql
@@ -1,3 +1,5 @@
--- V2__alter_sample_table.sql にリネームして中身を以下に変更
-ALTER TABLE sample
-  ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+CREATE TABLE sample (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
