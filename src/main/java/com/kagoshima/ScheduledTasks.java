365d4fcef615143996f6e2ba9e22374e97457844
takuhokago <takku1226000@gmail.com>
2025-06-19 20:25:02 +0900

    revert mail test code

--- src/main/java/com/kagoshima/ScheduledTasks.java
+++ src/main/java/com/kagoshima/ScheduledTasks.java
@@ -22,73 +22,75 @@ import jakarta.annotation.PostConstruct;
 @Component
 public class ScheduledTasks {
 
-    private final EmailService emailService;
-    private final ReportService reportService;
-    private final EmployeeService employeeService;
-    private final MessageSource messageSource;
+	private final EmailService emailService;
+	private final ReportService reportService;
+	private final EmployeeService employeeService;
+	private final MessageSource messageSource;
 
-    @Autowired
-    public ScheduledTasks(EmailService emailService, ReportService reportService, EmployeeService employeeService, MessageSource messageSource) {
-        this.emailService = emailService;
-        this.reportService = reportService;
-        this.employeeService = employeeService;
-        this.messageSource = messageSource;
-    }
+	@Autowired
+	public ScheduledTasks(EmailService emailService, ReportService reportService, EmployeeService employeeService,
+			MessageSource messageSource) {
+		this.emailService = emailService;
+		this.reportService = reportService;
+		this.employeeService = employeeService;
+		this.messageSource = messageSource;
+	}
 
-//    @Scheduled(cron = "0 0 0 27-31 * ?")
-    @PostConstruct
-    public void performTask() {
-//    	if(!isPreviousDayOfDeadline()) {
-//    		return;
-//    	}
-    	
-        ArrayList<Employee> toList = new ArrayList<>();
+	@Scheduled(cron = "0 0 0 27-31 * ?")
+	public void performTask() {
+		if (!isPreviousDayOfDeadline()) {
+			return;
+		}
 
-        List<Employee> generalEmployees = employeeService.findByRole(Employee.Role.GENERAL);
-        for (Employee employee : generalEmployees) {
-            List<Report> reports = reportService.findByEmployee(employee);
-            Report currentReport = null;
-            if(reports != null && !reports.isEmpty()) {
-                currentReport = reports.stream().max((r1, r2) -> r1.getReportMonth().compareTo(r2.getReportMonth())).get();
-            }
+		ArrayList<Employee> toList = new ArrayList<>();
 
-            if(currentReport == null || !checkCurrentReportCompleted(currentReport)) {
-                toList.add(employee);
-            }
-        }
-        sendEmail(toList);
-    }
+		List<Employee> generalEmployees = employeeService.findByRole(Employee.Role.GENERAL);
+		for (Employee employee : generalEmployees) {
+			List<Report> reports = reportService.findByEmployee(employee);
+			Report currentReport = null;
+			if (reports != null && !reports.isEmpty()) {
+				currentReport = reports.stream().max((r1, r2) -> r1.getReportMonth().compareTo(r2.getReportMonth()))
+						.get();
+			}
 
-    private void sendEmail(ArrayList<Employee> toList) {
-        String month = String.valueOf(YearMonth.now().getMonthValue());
-        String subject = messageSource.getMessage("subject.remind", new Object[] {month}, Locale.JAPAN);
+			if (currentReport == null || !checkCurrentReportCompleted(currentReport)) {
+				toList.add(employee);
+			}
+		}
+		sendEmail(toList);
+	}
 
-        for (Employee emp : toList) {
-        	String body = messageSource.getMessage("body.remind", new Object[] {emp.getFullName(),month}, Locale.JAPAN);
-            emailService.sendSimpleEmail(emp.getEmail(), subject, body);
-        }
-    }
+	private void sendEmail(ArrayList<Employee> toList) {
+		String month = String.valueOf(YearMonth.now().getMonthValue());
+		String subject = messageSource.getMessage("subject.remind", new Object[] { month }, Locale.JAPAN);
 
-    // 当月分の報告書が作成済みか
-    private boolean checkCurrentReportCompleted(Report report) {
-        boolean isCurrent = false;
-        boolean isCompleted = false;
+		for (Employee emp : toList) {
+			String body = messageSource.getMessage("body.remind", new Object[] { emp.getFullName(), month },
+					Locale.JAPAN);
+			emailService.sendSimpleEmail(emp.getEmail(), subject, body);
+		}
+	}
 
-        YearMonth currentMonth = YearMonth.now();
-        isCurrent = currentMonth.equals(report.getReportMonth());
+	// 当月分の報告書が作成済みか
+	private boolean checkCurrentReportCompleted(Report report) {
+		boolean isCurrent = false;
+		boolean isCompleted = false;
 
-        isCompleted = report.isCompleteFlg();
+		YearMonth currentMonth = YearMonth.now();
+		isCurrent = currentMonth.equals(report.getReportMonth());
 
-        return isCurrent && isCompleted;
-    }
-    
-    // 提出期日（月末）の前日であるか
-    private boolean isPreviousDayOfDeadline() {
-    	LocalDate now = LocalDate.now();
-    	YearMonth yearMonth = YearMonth.of(now.getYear(), now.getMonth());
-    	LocalDate lastDayOfMonth = yearMonth.atEndOfMonth();
-        boolean isPreviousDayOfDeadline = now.equals(lastDayOfMonth);
-    	return isPreviousDayOfDeadline;
-    }
+		isCompleted = report.isCompleteFlg();
+
+		return isCurrent && isCompleted;
+	}
+
+	// 提出期日（月末）の前日であるか
+	private boolean isPreviousDayOfDeadline() {
+		LocalDate now = LocalDate.now();
+		YearMonth yearMonth = YearMonth.of(now.getYear(), now.getMonth());
+		LocalDate lastDayOfMonth = yearMonth.atEndOfMonth();
+		boolean isPreviousDayOfDeadline = now.equals(lastDayOfMonth);
+		return isPreviousDayOfDeadline;
+	}
 
 }
