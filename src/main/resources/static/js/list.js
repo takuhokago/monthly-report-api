7d8f98bd7ec3bace9f482db4e8fd279ac9def74f
takuhokago <takku1226000@gmail.com>
2026-01-28 16:02:56 +0900

    いらないもの削除

--- src/main/resources/static/js/list.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('selectDate').addEventListener('change', function() {
-    var selectedValue = this.value;
-    document.getElementById('selectedDate').value = selectedValue;
-    document.getElementById('dateForm').submit();
-});
